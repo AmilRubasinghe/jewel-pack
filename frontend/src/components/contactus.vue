2d51e7308798c50af5d197d585abf6f9f1e173f1
Thiranjaya Munasinghe <thiranjayamunasinghe@gmail.com>
2019-09-01 20:05:26 +0530

    Contact Us frontend done

--- frontend/src/components/contactus.vue
+++ frontend/src/components/contactus.vue
@@ -1,40 +1,102 @@
 <template>
   <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-bottom justify-center>
-          <v-flex xs12 sm8 md8 v-for="i in 2" :key="`6${i}`" xs6></v-flex>
-          <v-flex xs12 sm8 md8 xs6>
-            <v-card class="elevation-10">
-              <v-toolbar color="bg-info" dark flat>
-                <v-toolbar-title class="tertiary--text font-weight-light">Contact us</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="Full Name"
-                    name="Full Name"
-                    prepend-icon="person"
-                    type="text"
-                  ></v-text-field>
-                  <v-text-field label="Email" name="Email" prepend-icon="email" type="email"></v-text-field>
+    <v-container grid-list-md ma-0>
+      <h1>Contact us</h1>
+      <v-layout wrap align-top justify-center>
+        <v-flex xs12 lg9>
+          <div class="resp-container">
+            <div class="gmap_canvas">
+              <iframe
+                class="resp-iframe"
+                src="https://maps.google.com/maps?q=119%20Gangarama%20Rd%20Boralesgamuwa&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                frameborder="0"
+                scrolling="no"
+                marginheight="0"
+                marginwidth="0"
+              ></iframe>
+              <a href="https://www.embedgooglemap.net/blog/best-wordpress-themes/"></a>
+            </div>
+          </div>
+        </v-flex>
 
-                  <v-textarea
-                    class="purple-input"
-                    label="Message"
-                    prepend-icon="message"
-                    type="text"
-                  />
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn>Send Message</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+        <v-flex xs12 lg3>
+          <h3>Address</h3>
+          <h5>
+            119/2, Gangarama Road, Werahera, Boralesgamuwa.
+            <br>
+            Mob: +94 777 374777, + 94 714 167 943
+            <br>
+            Tel: +94 11 2509089, +94 11 2517441
+          </h5>
+        </v-flex>
+      </v-layout>
+      <br />
+      <v-card  >
+            <v-card-title>
+              <h3>
+                For More Information
+              </h3>
+              </v-card-title>
+
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Full Name" name="Full Name" prepend-icon="person" type="text"></v-text-field>
+            <v-text-field label="Email" name="Email" prepend-icon="email" type="email"></v-text-field>
+            <v-text-field label="Phone" name="Phone" prepend-icon="phone" type="text"></v-text-field>
+
+            <v-textarea class="purple-input" label="Message" prepend-icon="message" type="text" />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn>Send Message</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
   </v-app>
 </template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {}
+};
+</script>
+
+
+
+
+<style>
+.mapouter {
+  position: relative;
+  text-align: right;
+  height: 400px;
+  width: 1024px;
+}
+.gmap_canvas {
+  overflow: hidden;
+  background: none !important;
+}
+
+.resp-container {
+  position: relative;
+  overflow: hidden;
+  padding-top: 56.25%;
+}
+
+.resp-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+</style>
+
--- frontend/src/components/test.vue
+++ frontend/src/components/test.vue
@@ -1,69 +1,24 @@
 <template>
-  <div>
-    <h1>Find Us</h1>
-    <div class="mapouter">
-      <div class="gmap_canvas">
-        <iframe
-          width="400"
-          height="400"
-          id="gmap_canvas"
-          src="https://maps.google.com/maps?q=119%20Gangarama%20Rd%2C%20Boralesgamuwa%2C%20Western%20Province&t=&z=11&ie=UTF8&iwloc=&output=embed"
-          frameborder="0"
-          scrolling="no"
-          marginheight="0"
-          marginwidth="0"
-        ></iframe>
-        <a href="https://www.pureblack.de">Pureblack.de - Website erstellen lassen</a>
-      </div>
-    </div>
-  </div>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div> {{ card_text }} </div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
-
-
-<script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-      colours: ["White", "Black", "Grey"],
-      border: "border String"
-    };
-  },
-
-  methods: {
-    send() {
-      console.log("sending");
-
-      axios
-        .post(this.$baseUrl + "/test", {
-          c: this.colours,
-          b: this.border
-        })
-        .then(response => {
-          console.log(response.data);
-
-          //console.log(this.slideshowItems);
-        })
-        .catch(error => {
-          console.log(error.response);
-          console.log("ERROR");
-        });
-    }
-  }
-};
-</script>
-
-<style>
-.mapouter {
-  position: relative;
-  text-align: right;
-  height: 400px;
-  width: 400px;
-}
-.gmap_canvas {
-  overflow: hidden;
-  background: none !important;
-  height: 400px;
-  width: 400px;
-}
-</style>
